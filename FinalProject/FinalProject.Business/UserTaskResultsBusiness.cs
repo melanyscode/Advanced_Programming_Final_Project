ef93128eda1557acee72618181ccf2cac4c499c5
Anny <annieacmg17@gmail.com>
2025-08-11 23:37:06 -0600

    UserTaskResult

---
+++ FinalProject/FinalProject.Business/UserTaskResultsBusiness.cs
@@ -0,0 +1,49 @@
+﻿using Repository;
+using System.Collections.Generic;
+using FinalProject.Data;
+
+namespace FinalProject.Business
+{
+    public class UserTaskResultsBusiness
+    {
+        private readonly RepositoryUserTaskResults repositoryUserTaskResults;
+
+        public UserTaskResultsBusiness()
+        {
+            repositoryUserTaskResults = new RepositoryUserTaskResults();
+        }
+
+        public IEnumerable<UserTaskResults> GetUserTaskResults()
+        {
+            return repositoryUserTaskResults.GetAll();
+        }
+
+        public UserTaskResults GetById(int id)
+        {
+            return repositoryUserTaskResults.GetById(id);
+        }
+
+        public void SaveUserTaskResult(int id, UserTaskResults userTaskResult)
+        {
+            if (id <= 0)
+                repositoryUserTaskResults.Add(userTaskResult);
+            else
+                repositoryUserTaskResults.Update(userTaskResult);
+        }
+
+        public void DeleteUserTaskResult(int id)
+        {
+            repositoryUserTaskResults.Delete(id);
+        }
+
+        public void SaveChanges()
+        {
+            repositoryUserTaskResults.Save();
+        }
+
+        public void Update(UserTaskResults userTaskResult)
+        {
+            repositoryUserTaskResults.Update(userTaskResult);
+        }
+    }
+}
