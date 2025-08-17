6c7f898fba20eff903d82cfeb5d67193d2c38530
isagutierrez12 <148836439+isagutierrez12@users.noreply.github.com>
2025-08-17 14:02:04 -0600

    Update RoleFilter.cs

--- FinalProject/FinalProject.Mvc/Filter/RoleFilter.cs
+++ FinalProject/FinalProject.Mvc/Filter/RoleFilter.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace AdvancedProgramming.Mvc.Filters
+namespace FinalProject.Mvc.Filters
 {
     public class RoleFilter : ActionFilterAttribute
     {
